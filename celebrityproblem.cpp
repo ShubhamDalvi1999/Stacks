ea2076980166c429e5e8d52b1347c51e4f621d43
ShubhamDalvi1999 <dshubhamp1999@gmail.com>
2022-08-04 01:21:43 +0530

    new code added

---
+++ celebrityproblem.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        // code here 
+        int ans=-1;
+        int count=0;//to count if all know celebrity
+        
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+            for(int c:M[i])// if ith person doesnot know anyone then sum=0
+                sum+=c;
+                
+            if( sum == 0 )
+            {  
+                ans=i;
+              
+                for(int j=0;j<n;j++)
+                {
+                    if(M[j][ans]==1)
+                    {
+                        count+=1;
+                    }
+                }
+                if(count==n-1)
+                    return i;
+            }
+        }
+        
+        return -1;
+     
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > M( n , vector<int> (n, 0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>M[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.celebrity(M,n)<<endl;
+
+    }
+}
+
+// } Driver Code Ends
