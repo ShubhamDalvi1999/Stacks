bf10dd49aaafcaea882fff7f77ab25015be88e8c
ShubhamDalvi1999 <dshubhamp1999@gmail.com>
2022-08-03 23:04:52 +0530

    new code

---
+++ paranthesischeck.cpp
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        // Your code here
+        stack<char> st;
+        for(char c: x)
+        {
+            if(c=='(')
+            {
+                st.push(c);
+            }
+            else if(c=='{')
+            {
+                st.push(c);
+            }
+            else if(c=='[')
+            {
+                st.push(c);
+            }
+            else if(c==']')
+            {
+                if(st.empty())      //cannot pop if st is empty
+                {
+                    return 0;
+                }
+                
+                if(st.top() == '[') //check for valid scenario
+                {
+                    st.pop();
+                }
+                else                //check for invalid scenario
+                    return false;
+                
+            }
+            else if(c=='}')
+            {
+                if(st.empty())
+                {
+                    return 0;
+                }
+                
+                if(st.top()=='{')
+                {
+                    st.pop();
+                }
+                else
+                    return false;
+                
+            }
+            else if(c==')')
+            {
+                if(st.empty())
+                {
+                    return 0;
+                }
+                
+                if(st.top()=='(')
+                {
+                    st.pop();
+                }
+                else
+                    return false;
+                
+            }
+        }
+        if(st.empty())
+        {
+            return true;
+        }
+        else
+            return false;
+        
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   int t;
+   string a;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       Solution obj;
+       if(obj.ispar(a))
+        cout<<"balanced"<<endl;
+       else
+        cout<<"not balanced"<<endl;
+   }
+}
+// } Driver Code Ends
